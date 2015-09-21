e50f0ba833d8b028eb02e48aeaf47509c0d51cea
CodingYue <this.isssssyy@gmail.com>
2015-09-21 22:50:53 +0800

    new version of shell.c

--- OS/shell/shell.c
+++ OS/shell/shell.c
@@ -1,24 +1,90 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 
 #define MAX_LINE 80
 
+void readcommand(size_t *args_size, int *need_to_wait, char **args) {
+
+	char buffer_str[MAX_LINE];
+	char *buffer = buffer_str;
+
+
+	while (1) {
+		char inputchr = getchar();
+
+		if (inputchr == ' ' || inputchr == '\n') {
+
+			*buffer = 0;
+			if (strlen(buffer_str) != 0) {
+				args[*args_size] = malloc(strlen(buffer_str)*sizeof(char));
+				memcpy(args[*args_size], buffer_str, strlen(buffer_str)*sizeof(char));
+				++(*args_size);
+				buffer = buffer_str;
+			}
+			if (inputchr == '\n') break;
+			continue;
+		}
+		*buffer = inputchr;
+		buffer++;
+	}
+
+
+	if (*args_size && strcmp(args[*args_size-1], "&") == 0) {
+		--*args_size;
+		*need_to_wait = 0;
+	}
+	args[*args_size] = NULL;
+}
 
 int main(void) {
 
-
 	char *args[MAX_LINE / 2 + 1];
-
-	bool should_run = true;
+	int should_run = 1;
 
 
 	while (should_run) {
 		printf("osh> ");
 		fflush(stdout);
 
+		size_t args_size = 0;
+		int need_to_wait = 1;
+
+		readcommand(&args_size, &need_to_wait, args);
+
+		if (args_size == 0) {
+			continue;
+		}
+
+		printf("need_to_wait = %d\n", need_to_wait);
+		fflush(stdout);
+
+
+		pid_t pid = fork(); 
+
+		if (pid < 0) {
+			fprintf(stderr, "Fork fail");
+			return 1;
+		}
+		else if (pid == 0) { // child process
+			char str[MAX_LINE] = "/bin/";
+			strcat(str, args[0]);
+
+			//puts(str);
+			execvp(str, args);
+			exit(0);
+			
+		} else {
+			if (need_to_wait) wait(NULL);
+			
+			//printf("Child Complete\n");
+		}
 
 	}
+
 
 	return 0;
 }
