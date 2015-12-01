52d20fcc10d808ddd394ee2cdcafbfe6cfc8994c
Yue Yang <this.isssssyy@gmail.com>
2015-12-01 13:15:20 +0800

    Computer Vision initial commmit

---
+++ computer-vision/main.cpp
@@ -0,0 +1,16 @@
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+int main(int, char** argv)
+{
+	// Load the image
+	Mat src = imread(argv[1]);
+	// Check if everything was fine
+	if (!src.data)
+		return -1;
+
+	imshow("Source Image", src);
+	return 0;
+}
